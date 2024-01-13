ceac0fab3366be10dbabb313ef7e5ede98f68dd3
Дмитрий Матохин <argizol@mail.ru>
2024-01-13 15:45:13 +0300

    проверка работы строки подключения из Appsettings.json

--- Store/Migrations/20240113124210_FourthCreate.Designer.cs
+++ Store/Migrations/20240113124210_FourthCreate.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetStore.Models;
 
@@ -10,9 +11,11 @@ using NetStore.Models;
 namespace Store.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20240113124210_FourthCreate")]
+    partial class FourthCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
